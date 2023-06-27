46486a8d7b322870b493445a4087f11d7bb5c0cf
bjohn <bjohn@rocketlawyer.co.uk>
2023-06-27 13:48:55 +0100

    Add motd

--- motd/recipes/default.rb
+++ motd/recipes/default.rb
@@ -7,3 +7,8 @@ cookbook_file '/etc/motd' do
   mode                       '0644'
   source                     'motd'
 end
+
+cookbook_file '/etc/profile.d/motd.sh' do
+  mode                       '0755'
+  source                     'motd.sh'
+end
--- motd/test/smoke/default/default_test.rb
+++ motd/test/smoke/default/default_test.rb
@@ -3,3 +3,11 @@ describe file('/etc/motd') do # rubocop:disable Layout/EmptyLineAfterMagicCommen
   it { should be_file }
   its('content') { should match(/.+/)}
 end
+
+describe file('/etc/profile.d/motd.sh') do # rubocop:disable Layout/EmptyLineAfterMagicComment
+  it { should be_file }
+end
+
+describe command("bash --login -i -c 'cd ~'") do
+  its('stdout') { should match('SIMULATED CHEF NODE') }
+end
