d1271f29331b4d2630f335091e38f6167acccb88
bjohn <bjohn@rocketlawyer.co.uk>
2023-06-27 13:24:56 +0100

    Add motd

--- motd/test/smoke/default/default_test.rb
+++ motd/test/smoke/default/default_test.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 describe file('/etc/motd') do # rubocop:disable Layout/EmptyLineAfterMagicComment
   it { should be_file }
+  its('content') { should match(/.+/)}
 end
